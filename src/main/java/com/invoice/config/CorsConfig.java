f7dbc18b70da46d01cfba6227ab47a33fc66980b
Jagrit Taneja <jagrit_taneja@intuit.com>
2026-05-10 14:47:31 +0530

    Added frotend and backend urls on cors config

--- src/main/java/com/invoice/config/CorsConfig.java
+++ src/main/java/com/invoice/config/CorsConfig.java
@@ -12,7 +12,8 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         CorsConfiguration config = new CorsConfiguration();
-
+        config.addAllowedOrigin("https://invoice-management-5c5t.onrender.com");
+        config.addAllowedOrigin("https://invoice-frontend-n9v6.onrender.com");
         config.addAllowedOrigin("http://localhost:3000");  // React dev server
         config.addAllowedOrigin("https://invoice-management.vercel.app"); // production
         config.addAllowedMethod("*");   // GET, POST, PUT, DELETE, PATCH
